cf9ae03d6390ab7728666c05b839c503c25aeb34
pooja <poojigupta71@gmail.com>
2025-08-25 13:10:27 +0530

    Make add to cart button interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,32 +1,4 @@
-console.log('hello');
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name:' Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count : 87
-    },
-    price : 1090
-},  {
-    image : 'images/products/intermediate-composite-basketball.jpg',
-    name : 'Intermediate Size Basketball',
 
-    rating : {
-        stars : 4,
-        count : 125
-    },
-
-    price :  2095
-    
-},  {
-    image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating : {
-        stars : 4.5,
-        rating : 145
-    },
-    price :  799
-}];
 
 let productsHtml = ''
 
@@ -76,7 +48,9 @@ products.forEach((product) => {
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button
+             button-primary js-add-to-cart"
+             data-product-id="${product.id}">
                 Add to Cart
             </button>
             </div>
@@ -84,9 +58,35 @@ products.forEach((product) => {
    `;
 });
 
-console.log(productsHtml);
 
+console.log('hello');
 document.querySelector('.js-product-grid').
-        innerHTML = productsHtml
+        innerHTML = productsHtml;
 
+
+document.querySelectorAll('.js-add-to-cart')
+        .forEach((button) => {
+            button.addEventListener('click' , () => {
+              const productId = button.dataset.productId;
+   
+              let matchingItem;
+
+              cart.forEach((item) => {
+               if(productId === item.productId){
+                  matchingItem = item;
+               }
+              });
+
+              if(matchingItem){
+                matchingItem.quantity += 1;
+              }
+              else{
+                cart.push({
+                    productId : productId,
+                    quantity : 1
+                });
+              }
+              console.log(cart);
+            });
+        });
 
