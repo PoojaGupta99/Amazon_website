efb6b4b5ebf3fd7182ad430af8d4d62ef0549904
pooja <poojigupta71@gmail.com>
2025-08-28 09:10:11 +0530

    Save  all chnages

---
+++ javascript-amazon-project-main/data/deliveryOptions.js
@@ -0,0 +1,17 @@
+export const deliveryOptions = [
+  {
+    id: '1',
+    deliveryDays: 7, //  correct key
+    priceCents: 0      //  correct key
+  },
+  {
+    id: '2',
+    deliveryDays: 3,   //  correct key
+    priceCents: 499
+  },
+  {
+    id: '3',
+    deliveryDays: 1,   //  correct key
+    priceCents: 999
+  }
+];
--- javascript-amazon-project-main/scripts/utils/money.js
+++ javascript-amazon-project-main/scripts/utils/money.js
@@ -1,3 +1,4 @@
  export function formatcurrency(PriceCents) {
     return ((PriceCents/100) .toFixed(2));
 }
+export default formatcurrency;
