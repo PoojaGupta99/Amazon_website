830e950c01d38dc193ebd72e67b1c8c228304864
pooja <poojigupta71@gmail.com>
2025-08-30 16:14:15 +0530

    Use fetch

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -63,6 +63,29 @@ export function getProduct(productId) {
 export  let products = [];
 
 
+ export function loadProductsFetch() {
+  const promise = fetch('https://supersimplebackend.dev/products').then((response) => {
+  return  response.json()
+  }).then((productData) => {
+        products = productData.map((productDetails) => {
+
+        if(productDetails.type === 'clothing') {
+              return new Clothing(productDetails);
+        }
+        return new Product(productDetails);
+      });
+
+      console.log('loadn products');
+  });
+  return promise;
+}
+
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+})
+*/
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest()
 
